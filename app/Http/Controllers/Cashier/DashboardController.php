a47ab8b5e7074ca37619c70336fd6adf14580a62
zin thu <zinthuaceplus47@gmail.com>
2018-03-09 16:28:20 +0630

    resolve conflict

--- app/Http/Controllers/Cashier/DashboardController.php
+++ app/Http/Controllers/Cashier/DashboardController.php
@@ -29,26 +29,6 @@ class DashboardController extends Controller
 
     protected function getDayStart() 
     {
-<<<<<<< HEAD
-        $member     = Member::all()->count();
-        $set        = SetMenu::where('status',1)->count();
-        $item       = Item::where('status',1)->count();
-        $category   = Category::where('status',1)->count();
-        $order_paid_status      = StatusConstance::ORDER_PAID_STATUS;
-
-        $orders = Order::select(DB::raw('DATE_FORMAT(order_time,"%b") as month'),DB::raw('sum(all_total_amount) as total'))
-        ->where('order_time','>=',DB::raw('DATE_ADD(now(),INTERVAL - 12 MONTH)'))
-        ->where('status',$order_paid_status)->whereYear('order_time','=',date('Y'))
-        ->groupBy(DB::raw('MONTH(order_time)'))->get();    
-        
-        $daily_order = Order::select(DB::raw('DATE(order_time) as date'),DB::raw('sum(all_total_amount) as total'))->groupBy(DB::raw('Day(order_time)'))
-        ->whereMonth('order_time','=',date('m'))->whereYear('order_time','=',date('Y'))
-        ->where('status',$order_paid_status)->limit(7)->orderBy('date','desc')->get();    
-        
-        return view('Backend.dashboard.dashboard')->with('member',$member)
-            ->with('set',$set)->with('item',$item)->with('category',$category)->with('orders',$orders)
-            ->with('daily_order',$daily_order);
-=======
         $session_status     = StatusConstance::DAY_STARTING_STATUS;
         $daystart           = DayStart::select('id','start_date','status')
                               ->where('status',$session_status)
@@ -108,7 +88,6 @@ class DashboardController extends Controller
         $get_day->daystart    = $daystart;
         $get_day->shift       = $shiftObj;
         return $get_day;
->>>>>>> theodore
     }
 
     public function authorized(){
