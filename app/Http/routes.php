6f1d944c53ef7fc24af1098398e901fd73fbe008
zin thu tun <zinthuaceplus47@gmail.com>
2019-01-19 12:19:40 +0630

    update api

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -782,3 +782,4 @@ Route::get('/stock/create','inventory\inventoryController@stock_item');
 Route::get('sync_um','inventory\inventoryController@getSyncUm');
 Route::get('api/v1/get_kitchen','inventory\inventoryController@getKitchen');
 Route::get('/remainvalance','inventory\inventoryController@getremainbalance');
+Route::get('/sale/stock/{id}','inventory\inventoryController@saleStock');
