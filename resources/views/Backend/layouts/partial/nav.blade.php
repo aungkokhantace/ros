fed8589f9f20e3cbfe6550c420a90d2e1315df0f
aungkokhant <aungkokhantace@gmail.com>
2019-01-20 09:00:22 +0630

    hide setmenu

--- resources/views/Backend/layouts/partial/nav.blade.php
+++ resources/views/Backend/layouts/partial/nav.blade.php
@@ -54,7 +54,7 @@
             <li><a href="/Backend/Item/index"><i class="fa fa-circle-o text-aqua"></i>  Items</a></li>
             <li><a href="/Backend/Continent/index"><i class="fa fa-circle-o text-aqua"></i> </i> Continent</a></li>
             <li><a href="/Backend/AddOn/index"><i class="fa fa-circle-o text-aqua"></i>  Add On</a></li>
-            <li><a href="/Backend/SetMenu/index"><i class="fa fa-circle-o text-aqua"></i> </i> Set Menu</a></li>
+            <!-- <li><a href="/Backend/SetMenu/index"><i class="fa fa-circle-o text-aqua"></i> </i> Set Menu</a></li> -->
 
           </ul>
         </li>
