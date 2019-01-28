f1f72c8655067b77c710383aa55c3df6dbfe54c3
Myo Thant Kyaw <myothantkyaw.bb19@gmail.com>
2019-01-28 12:11:37 +0630

    fix the bug for config

--- config/database.php
+++ config/database.php
@@ -60,7 +60,7 @@ return [
             'username'  => env('DB_USERNAME', 'root'),
 //            'password'  => env('DB_PASSWORD', ''),
 
-            'password'  => env('DB_PASSWORD', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
 
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
